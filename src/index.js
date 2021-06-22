12f80cfbab570f148291e8bababef34fd36f5f7d
Adam Ghaoul <adamghaoul@gmail.com>
2021-06-22 15:38:14 +0200

    first iteration

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+<div className="blue">
+<h1>Say hello to ReactJS</h1>
+<h3>You will learn how to use the most popular frontend library, and become a super Ninja developer.</h3>
+<h3> Awesome!</h3>
+
+</div>
+
+,document.getElementById('root') );
